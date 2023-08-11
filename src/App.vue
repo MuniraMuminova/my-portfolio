176f58c7966472976a59c6080e7c669b94e71cbe
Munira <munirashovokhobova@gmail.com>
2023-08-11 12:25:41 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="container">
+    <Navbar/>
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "./components/Navbar/Navbar.vue";
+import Footer from "./components/Footer/Footer.vue"
+
+</script>
+
+<style>
+
+
+</style>
---
+++ src/pages/Contacts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="contact">
+      <h2 class="contact__title">Мои контакты</h2>
+      <div class="contact__box">
+        <div class="contact__right">
+          <p class="contact__right-title">Связаться со мной</p>
+          <div class="contact__right-btns">
+            <button class="contact__right-btn">
+              <a href="tel:+998977098286">
+                <fa :icon="['fas', 'phone']" class="contact__right-icon" />
+                <span>Phone </span>
+              </a>
+            </button>
+            <button class="contact__right-btn">
+              <a href="https:/t.me/Shovokhobova">
+              <fa :icon="['fab', 'telegram']" class="contact__right-icon" />
+              <span> Telegram</span>
+            </a>
+            </button>
+            <button class="contact__right-btn">
+              <fa :icon="['fab', 'instagram']" class="contact__right-icon" />
+              <span> Instagram</span>
+            </button>
+            <button class="contact__right-btn">
+              <a href="mailto:munirashovokhobova@gmail.com">
+                <fa :icon="['fas', 'envelope']" class="contact__right-icon" />
+                <span> Mail</span>
+              </a>
+            </button>
+          </div>
+        </div>
+        <div class="contact__left">
+          <img src="../assets/images/calling.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
